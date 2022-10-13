4a22c7064da5d2c697dd18030940200e928fbd06
VirKato <dascat79@mail.ru>
2022-10-13 18:09:05 +0700

    поправки версий зависимостей

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/CarServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/CarServiceImp.java
@@ -3,9 +3,7 @@ package hiber.service;
 import hiber.dao.CarDao;
 import hiber.model.Car;
 import hiber.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
